c099320bedb9e79bf82c34be27e23e7d7c5e4e00
AnaMaghear <ana.maghear@yahoo.com>
2020-10-30 21:43:53 +0200

    2

--- Ana Maghear/main.py
+++ Ana Maghear/main.py
@@ -28,8 +28,39 @@ print(3 ** 4) # 81: ridicare la putere
 print(14 / 23) # 0.608: impartire
 print(45 // 4) # 11: returneaza un intreg
 
+integer_var = int("5")
+float_var = float ("6.8")
+print(type (integer_var))
+print(type (float_var))
+
+bool_var = False
+print(bool_var)
+
+elements = [3, 5, 7 ,True , "String here" , 7.0 ] # Lists are mutable
+print(elements)
+print(elements[4])
+elements.append("New item")
+print()
+print(elements)
+print(elements.insert(3, "Another item")) # add item before a specific index
+print (elements)
+
+elements = elements + ["Added item", 5, 7]
+print(elements)
+
+
 def function():
-    abracadabra = "something "
+    abracadabra = "something"
     print(abracadabra)
+
 function()
+print(elements[2:6]) # returneaza un substring de la poz 2 pana la 6 fara ultimul
+print(elements[:6])
+print(elements[1:])
+
+print(elements[3:9:3]) # elemente de l 3 la 9 din 3 in teri
+print(elements[-2:-5:-1])
+print(elements[5:2:-1])
+
+print(elements * 3)
 
