ad96aadf1f5a1ebed53b8e3805a6cc8f8ba17b00
Alex Stoianovici <astoianovici@ymail.com>
2020-10-30 21:05:40 +0200

    run math operations

--- Stoianovici/main.py
+++ Stoianovici/main.py
@@ -1,1 +1,34 @@
-print('Hello world!');
+print('Hello world')
+
+#tipuri built-in
+"""
+    - str = string
+    - chr = char
+    - int = integer
+    - float
+    - bool
+    - list = ordered and indexed list
+    - tuple = ordered and indexed lista nemodificabila
+    - set = unordered and unindexed list
+    - dict = dictionary, dictionar care contine perechi cheie - valoare
+"""
+
+cat = "Anakin"
+print(cat)
+
+#type = returneaza tipul variabilei
+print(type(cat))
+
+cat = 4
+print(type(cat))
+
+print(4 * 5)
+print(3 ** 4)
+print(14 / 23)
+print(45 // 3)
+
+
+
+
+
+
